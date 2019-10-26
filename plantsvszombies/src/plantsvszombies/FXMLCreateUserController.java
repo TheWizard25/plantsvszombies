a229992a87ee1c23b7564b563f364c7a425b9834
nitesh18400 <nitesh18400@iiitd.ac.in>
2019-10-26 14:45:28 +0530

    createbuttomtolevelpage

--- plantsvszombies/src/plantsvszombies/FXMLCreateUserController.java
+++ plantsvszombies/src/plantsvszombies/FXMLCreateUserController.java
@@ -5,9 +5,17 @@
  */
 package plantsvszombies;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -19,6 +27,20 @@ public class FXMLCreateUserController implements Initializable {
     /**
      * Initializes the controller class.
      */
+     @FXML
+    private void clicknext(ActionEvent event) throws IOException {
+    handleButtonAction(event, "FXMLlevelpage.fxml");
+   }
+
+    
+    private void handleButtonAction(ActionEvent event, String s) throws IOException{
+            Parent create_user_parent = FXMLLoader.load(getClass().getResource(s));
+            Scene create_user_scene = new Scene(create_user_parent);
+            Stage game_stage= (Stage) ((Node)event.getSource()).getScene().getWindow();
+            game_stage.setScene(create_user_scene);
+            game_stage.show();
+ 
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
