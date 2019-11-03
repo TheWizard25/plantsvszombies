9d66a48aadeecfc689b9828b996f35b6a6a21106
nitesh18400 <nitesh18400@iiitd.ac.in>
2019-11-03 13:54:43 +0530

    INGAMEMENU

---
+++ plantsvszombies/src/plantsvszombies/FXMLInGameMenuFXMLController.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plantsvszombies;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author nitesh
+ */
+public class FXMLInGameMenuFXMLController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    
+    @FXML
+    private void ingameresume(ActionEvent event) throws IOException 
+    {
+    handleButtonAction(event, "FXMLLawn.fxml");
+   }
+    @FXML
+    private void BACKTOMAINMENU(ActionEvent event) throws IOException 
+    {
+    handleButtonAction(event, "FXMLMainMenu.fxml");
+   }
+    private void handleButtonAction(ActionEvent event, String s) throws IOException{
+            Parent create_user_parent = FXMLLoader.load(getClass().getResource(s));
+            Scene create_user_scene = new Scene(create_user_parent);
+            Stage game_stage= (Stage) ((Node)event.getSource()).getScene().getWindow();
+            game_stage.setScene(create_user_scene);
+            game_stage.show();
+ 
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
--- plantsvszombies/src/plantsvszombies/FXMLLawnController.java
+++ plantsvszombies/src/plantsvszombies/FXMLLawnController.java
@@ -5,9 +5,21 @@
  */
 package plantsvszombies;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.animation.ParallelTransition;
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 
 /**
  * FXML Controller class
@@ -15,13 +27,54 @@ import javafx.fxml.Initializable;
  * @author HD
  */
 public class FXMLLawnController implements Initializable {
-
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private void InGameMenu(ActionEvent event) throws IOException 
+    {
+    handleButtonAction(event, "FXMLInGameMenuFXML.fxml");
+   }
+    private void handleButtonAction(ActionEvent event, String s) throws IOException{
+            Parent create_user_parent = FXMLLoader.load(getClass().getResource(s));
+            Scene create_user_scene = new Scene(create_user_parent);
+            Stage game_stage= (Stage) ((Node)event.getSource()).getScene().getWindow();
+            game_stage.setScene(create_user_scene);
+            game_stage.show();
+ 
+    }
+    @FXML
+    private ImageView lawnmover1;
+    @FXML
+    private ImageView Zombie;
+    @FXML
+    private ImageView Peamoving;
+    
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        TranslateTransition tt = new TranslateTransition();
+        tt.setDuration(Duration.seconds(5));
+        tt.setNode(lawnmover1);
+        tt.setToX(+600);
+        //tt.setCycleCount(1);
+        //tt.setAutoReverse(true);
+        
+        
+       TranslateTransition ll = new TranslateTransition();
+        ll.setDuration(Duration.seconds(1.4));
+        ll.setNode(Peamoving);
+        ll.setToX(+450);
+        ll.setCycleCount(100);
+        //tt.setAutoReverse(true);
+        TranslateTransition zz = new TranslateTransition();
+        zz.setDuration(Duration.seconds(20));
+        zz.setNode(Zombie);
+        zz.setToX(-450);
+        zz.setCycleCount(1);
+        tt.play();
+        ll.play();
+        zz.play();
+        
+        
+        
+    }      
     
 }
